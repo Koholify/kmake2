c7d170518e4599aba43efcd5208346cdfe2cd3b7
Kyle <keprice@mun.ca>
2024-03-13 13:23:37 -0230

    .

--- src/kmake.py
+++ src/kmake.py
@@ -5,6 +5,7 @@ import os
 from os import abort, path
 import shutil
 import sys
+from concurrent.futures import ThreadPoolExecutor
 
 CONFIG_FILE_NAME = "KMakeFile.txt"
 VERSION_NUMBER = "1.0.0"
@@ -208,6 +209,25 @@ def install_exe():
     except PermissionError:
         print(f"PermissionError: install failed: '{install_path}'")
 
+def compileFile(s_file, o_file, source_path) -> bool:
+        s_mtime = path.getmtime(path.join(source_path, s_file))
+        o_mtime = 0.0
+
+        try:
+            o_mtime = path.getmtime(o_file)
+        except FileNotFoundError:
+            pass
+
+        if s_mtime > o_mtime or header_mtime > o_mtime:
+            command = create_compile_command(cfg, s_file)
+            print(command)
+            res = os.system(command)
+            if not res == 0:
+                print(f"Error Compiling {s_file}")
+                return False
+
+        return True
+
 def compile():
     cfg = fill_config()
     source_path = cfg.d_src
@@ -225,22 +245,9 @@ def compile():
         header_mtime = max(list(map(lambda f: path.getmtime(f), header_files)))
 
     errors = False
+    threading.RLock
     for (s_file, o_file) in zip(source_files, target_objs):
-        s_mtime = path.getmtime(path.join(source_path, s_file))
-        o_mtime = 0.0
-
-        try:
-            o_mtime = path.getmtime(o_file)
-        except FileNotFoundError:
-            pass
-
-        if s_mtime > o_mtime or header_mtime > o_mtime:
-            command = create_compile_command(cfg, s_file)
-            print(command)
-            res = os.system(command)
-            if not res == 0:
-                print(f"Error Compiling {s_file}")
-                errors = True
+        pass
 
     if not errors:
         target = get_target_name(cfg)
